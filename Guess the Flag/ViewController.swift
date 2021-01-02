445258dd60268e94cdfe085ffef9e789623604e7
Vyacheslav Efimov <efimov@element-soft.com>
2021-01-02 16:16:13 +0300

    Initial Commit

---
+++ Guess the Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Вячеслав Ефимов on 02.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
